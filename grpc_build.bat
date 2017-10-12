01fac2bd10121ebfc68ef1be8077437e513038cf
Jake Wang <delight.wjk@gmail.com>
2017-10-12 13:05:01 +0800

    Updated cmake path

--- grpc_build.bat
+++ grpc_build.bat
@@ -3,7 +3,7 @@
 
 @REM EDIT THIS SECTION ACCORDING TO YOUR ENV
 call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" amd64
-set path=%path%;C:\Program Files\cmake-3.8.1-win64-x64\bin
+set path=%path%;C:\Program Files\CMake\bin
 set path=%path%;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE
 set path=%path%;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin
 @REM EOF
